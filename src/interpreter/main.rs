797a9b919875a5643c0d26541ac5b39561bfdb8c
swz-git <contact@swz.works>
2023-08-30 02:22:36 +0200

    use jump map

--- src/interpreter/main.rs
+++ src/interpreter/main.rs
@@ -1,6 +1,13 @@
 use clap::Parser;
+use nohash_hasher::NoHashHasher;
 use rsbflib::{BracketState, TokenKind, TokenValue};
-use std::fs;
+use std::{
+    collections::{BTreeMap, HashMap},
+    error::Error,
+    fs,
+    hash::BuildHasherDefault,
+    io::{self, Write},
+};
 
 /// Brainfuck interpreter
 #[derive(Parser, Debug)]
@@ -11,6 +18,8 @@ struct Args {
     file: String,
 }
 
+// caching was not worth it
+// #[cached(key = "usize", convert = "{ startpos }")]
 fn find_correct_closing_bracket(startpos: usize, tokens: &Vec<rsbflib::Token>) -> Option<usize> {
     let mut pos = startpos + 1;
     let mut non_closed_openings = 0;
@@ -44,6 +53,38 @@ fn find_correct_closing_bracket(startpos: usize, tokens: &Vec<rsbflib::Token>) -
     }
 }
 
+fn generate_jumping_map(
+    tokens: &Vec<rsbflib::Token>,
+) -> Result<HashMap<usize, usize, BuildHasherDefault<NoHashHasher<usize>>>, Box<dyn Error>> {
+    let mut map: HashMap<usize, usize, BuildHasherDefault<NoHashHasher<usize>>> =
+        HashMap::with_hasher(BuildHasherDefault::default());
+    let mut open_bracket_index_stack: Vec<usize> = vec![];
+    for (i, token) in tokens.iter().enumerate() {
+        match token.value {
+            TokenValue::BracketState(BracketState::Open) => open_bracket_index_stack.push(i),
+            TokenValue::BracketState(BracketState::Closed) => {
+                map.insert(
+                    open_bracket_index_stack
+                        .pop()
+                        .ok_or("Too many closing brackets")?,
+                    i,
+                );
+            }
+            _ => { /* We don't care */ }
+        }
+    }
+
+    if open_bracket_index_stack.len() > 0 {
+        Err("Too many opening brackets")?
+    }
+
+    Ok(map)
+    // Ok(map
+    //     .iter()
+    //     .map(|x| (*x.0, x.1.expect("Opened bracket never closes")))
+    //     .collect())
+}
+
 const MEM_SIZE: usize = 30000;
 
 fn interpret(tokens: Vec<rsbflib::Token>) {
@@ -52,12 +93,21 @@ fn interpret(tokens: Vec<rsbflib::Token>) {
     let mut pos: usize = 0;
     let mut loop_stack: Vec<usize> = vec![];
 
+    let mut stdout = io::stdout();
+    let mut temp_stdout_buf = [0u8; 1];
+
+    let jumping_map = generate_jumping_map(&tokens).expect("Couldn't generate jumping tables");
+
     while tokens.len() > pos {
         let token = &tokens[pos];
         match &token.value {
             TokenValue::None => match token.kind {
                 TokenKind::Output => {
-                    print!("{}", memory[mempos] as u8 as char);
+                    (memory[mempos] as u8 as char).encode_utf8(&mut temp_stdout_buf);
+                    stdout
+                        .write(&temp_stdout_buf)
+                        .expect("Couldn't write to stdout");
+                    stdout.flush().expect("fuck you");
                 }
                 TokenKind::Input => {
                     todo!("input char (,)")
@@ -80,18 +130,17 @@ fn interpret(tokens: Vec<rsbflib::Token>) {
             TokenValue::BracketState(s) => match s {
                 BracketState::Open => {
                     if memory[mempos] as u8 == 0 {
-                        // Takes 14167554488ns of mandelbrot when in release mode
-                        pos = find_correct_closing_bracket(pos, &tokens)
-                            .expect(&*format!("Opened loop never closed: token pos: {}", pos));
+                        pos = *jumping_map.get(&pos).expect("Opened loop never closed");
+
+                        // pos = find_correct_closing_bracket(pos, &tokens)
+                        //     .expect("Opened loop never closed");
                     } else {
                         loop_stack.push(pos);
                     }
                 }
                 BracketState::Closed => {
                     if memory[mempos] as u8 != 0 {
-                        pos = *loop_stack
-                            .last()
-                            .expect(&*format!("Closed loop never opened: token pos: {}", pos));
+                        pos = *loop_stack.last().expect("Closed loop never opened")
                     } else {
                         loop_stack.pop();
                     }
